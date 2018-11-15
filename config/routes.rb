804f37933cfb2f925b43cfafb0581c1d74988be2
Hai-Dang <haidang.hathuc@gmail.com>
2018-11-15 20:44:54 +0100

    ajout vue globale

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get  '/gotham', to: 'home#gotham'
+  get '/classic', to: 'home#classic'
+  get '/griezmann', to: 'home#griezmann'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

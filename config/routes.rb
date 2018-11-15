c1ccdc9b662ed142278d2ebce66577cad5ac402c
RomainRec <romain.recher@gmail.com>
2018-11-15 17:32:27 +0100

    route, controller et index gotham ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :home
   root 'home#index'
+
+  get  '/gotham', to: 'home#gotham'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
